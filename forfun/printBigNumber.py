beec49c0e3f824653f8130199f257f376f638fb2
wanglj <wanglj@gzyitop.com>
2018-01-12 18:11:13 +0800

    数字变大在控制台输出

---
+++ forfun/printBigNumber.py
@@ -0,0 +1,127 @@
+import sys
+
+ZERO = ['  ***  ',
+        ' *   * ',
+        '*     *',
+        '*     *',
+        '*     *',
+        ' *   * ',
+        '  ***  '
+        ]
+ONE = [' * ',
+       '** ',
+       ' * ',
+       ' * ',
+       ' * ',
+       ' * ',
+       '***'
+       ]
+TWO = ['  *** ',
+       ' *   *',
+       '*   * ',
+       '   *  ',
+       '  *   ',
+       ' *    ',
+       '******'
+       ]
+ThREE = ['  **** ',
+         ' *    *',
+         '      *',
+         '   ****',
+         '      *',
+         ' *    *',
+         '  **** '
+         ]
+FOUR = ['    * ',
+        '   ** ',
+        '  * * ',
+        ' *  * ',
+        '******',
+        '    * ',
+        '    * '
+        ]
+
+FIVE = ['*****',
+        '*    ',
+        '*    ',
+        '*****',
+        '    *',
+        '    *',
+        '*****'
+        ]
+
+SIX = ['*****',
+       '*    ',
+       '*    ',
+       '*****',
+       '*   *',
+       '*   *',
+       '*****'
+       ]
+SEVEN = ['****',
+         '   *',
+         '   *',
+         '   *',
+         '   *',
+         '   *',
+         '   *'
+         ]
+EIGHT = ['****',
+         '*  *',
+         '*  *',
+         '****',
+         '*  *',
+         '*  *',
+         '****'
+         ]
+NIGHT = ['****',
+         '*  *',
+         '*  *',
+         '****',
+         '   *',
+         '   *',
+         '****'
+         ]
+
+
+# 打印单个数字
+def printsingle(thenumber):
+    for item in thenumber:
+        print(item)
+
+
+# 打印数字数组
+def printall(allnumber):
+    count = len(allnumber)
+    row = 0
+    while row < 7:
+        start = 0
+        line = ""
+        while start < count:
+            line += allnumber[start][row] + "  "
+            start = start + 1
+            if start == count:
+                print(line)
+        row = row + 1
+
+
+digital = [ZERO, ONE, TWO, ThREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NIGHT]
+# printall(digital)
+# inputnumber=input("请输入一串数字：")
+# dic=sys.argv[0]
+
+try:
+    inputnumber = input("请输入一串数字：")
+    count = len(inputnumber)
+    row = 0
+    while row < 7:
+        start = 0
+        line = ""
+        while start < count:
+            line += digital[int(inputnumber[start])][row] + "  "
+            start = start + 1
+            if start == count:
+                print(line)
+        row = row + 1
+except IndexError as error:
+    print(error)
