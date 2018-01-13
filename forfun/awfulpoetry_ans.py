ed3d3da230e21e0c99047ad179ec456ed9c317ac
wanglj <wanglj@gzyitop.com>
2018-01-13 15:34:15 +0800

    求解一元二次方程，以及将csv转为html

--- forfun/awfulpoetry_ans.py
+++ forfun/awfulpoetry_ans.py
@@ -3,20 +3,20 @@ import random
 
 articles = ['a', 'the']  # 冠词
 nouns = ['dog', 'man', 'woman', 'cat', 'donkey', 'bird']  # 名词
-verbs = ['sang', 'ran', 'jumped','laughed']  # 动词
+verbs = ['sang', 'ran', 'jumped', 'laughed']  # 动词
 adverbial = ['loudly', 'quietly', 'well', 'badly']  # 状语
 str = input("请输入希望生成的诗歌行数：")
 try:
     rows = int(str)
     row = 0
     while row < rows:
-        sentencestype = random.randint(1, 2)        # 选择句子结构
+        sentencestype = random.randint(1, 2)  # 选择句子结构
         if (sentencestype == 1):
             print(random.choice(articles) + " " + random.choice(nouns) + " " + random.choice(verbs))  # 冠词+名词+动词
         else:
-            print(          # 冠词+名词+动词+状语
+            print(  # 冠词+名词+动词+状语
                 random.choice(articles) + " " + random.choice(nouns) + " " + random.choice(verbs) + " " + random.choice(
                     adverbial))
-        row+=1
+        row += 1
 except ValueError as err:
     print(err)
